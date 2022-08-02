964f03e118c55aa18956ba143f48b39c792336e2
Valkje <loran.knol@gmail.com>
2022-08-02 16:45:27 +0200

    Make 'likable' spelling consistent

--- database/migrations/2022_06_08_212638_create_likable_likes_table.php
+++ database/migrations/2022_06_08_212638_create_likable_likes_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('likeable_likes', function (Blueprint $table) {
+        Schema::create('likable_likes', function (Blueprint $table) {
             $table->id();
             $table->string('likable_id', 36);
             $table->string('likable_type', 100);
             $table->string('user_id', 36)->index();
             $table->timestamps();
-            $table->unique(['likable_id', 'likable_type', 'user_id'], 'likeable_likes_unique');
+            $table->unique(['likable_id', 'likable_type', 'user_id'], 'likable_likes_unique');
         });
     }
 
