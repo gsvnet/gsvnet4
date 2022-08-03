3ba56b051fafb45429b0ebb170b5f8748a08c36d
Valkje <loran.knol@gmail.com>
2022-08-03 22:05:36 +0200

    Create UserProfile factory

--- database/factories/UserProfileFactory.php
+++ database/factories/UserProfileFactory.php
@@ -9,6 +9,10 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class UserProfileFactory extends Factory
 {
+    private array $studies = ['Wiskunde', 'Geschiedenis', 'Tandheelkunde', 'IB/IO', 'Bedrijfskunde'];
+    private array $companies = ['Gadero', 'Bo & Co', 'Mevrouw Verhagen', null];
+    private array $professions = ['Schoonmaker', 'Putjesschepper', null];
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,27 @@ class UserProfileFactory extends Factory
     public function definition()
     {
         return [
-            //
+            // user_id?
+            // year_group_id?
+            'initials' => strtoupper($this->faker->lexify('??')),
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->streetAddress,
+            'zip_code' => $this->faker->postcode,
+            'town' => $this->faker->city,
+            'country' => $this->faker->country,
+            'study' => $this->faker->randomElement($this->studies),
+            'birthdate' => $this->faker->dateTimeThisCentury->format('Y-m-d'),
+            'gender' => rand(0,1),
+            'student_number' => 's' . (string) rand(1000000, 5000000),
+            'parent_address' => $this->faker->streetAddress,
+            'parent_zip_code' => $this->faker->postcode,
+            'parent_town' => $this->faker->city,
+            'parent_phone' => $this->faker->phoneNumber,
+            'parent_email' => $this->faker->safeEmail,
+            'inauguration_date' => $this->faker->dateTimeBetween('-10 years', 'now'),
+            'company' => $this->faker->randomElement($this->companies),
+            'profession' => $this->faker->randomElement($this->professions),
+            'business_url' => $this->faker->url
         ];
     }
 }
