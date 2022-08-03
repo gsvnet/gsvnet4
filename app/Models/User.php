66a285340f9a70435b92e5675697b1c0517a576d
Valkje <loran.knol@gmail.com>
2022-08-03 22:03:55 +0200

    Remove @property

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,9 +13,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-/**
- * @property mixed $type
- */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
