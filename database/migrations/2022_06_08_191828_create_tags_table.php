e881eb3e238bcf475040add3f68d935144060777
Valkje <loran.knol@gmail.com>
2022-08-05 19:07:49 +0200

    Remove redundant columns from tags table

--- database/migrations/2022_06_08_191828_create_tags_table.php
+++ database/migrations/2022_06_08_191828_create_tags_table.php
@@ -18,8 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug');
             $table->text('description');
-            $table->boolean('forum');
-            $table->boolean('articles');
             $table->timestamps();
         });
     }
