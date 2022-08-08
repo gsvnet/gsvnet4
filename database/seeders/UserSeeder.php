5e641156e25be48c3c03a819b9eab99d72761c7a
Valkje <loran.knol@gmail.com>
2022-08-08 13:25:51 +0200

    Add admin user to seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Committee;
 use App\Models\User;
+use Carbon\Carbon;
 use Faker\Factory;
+use GSVnet\Users\UserType;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -22,6 +25,22 @@ class UserSeeder extends Seeder
         $yearGroupIds = DB::table('year_groups')->pluck('id');
         $faker = Factory::create(config('app.faker_locale'));
 
+        // Admin user
+        $loran = User::factory()
+            ->hasProfile(1, ['year_group_id' => $faker->randomElement($yearGroupIds)])
+            ->create([
+                'firstname' => 'Loran',
+                'middlename' => '',
+                'lastname' => 'Knol',
+                'username' => 'Valkje',
+                'email' => 'loran.knol@gmail.com',
+                'type' => UserType::MEMBER,
+                'approved' => true
+            ]);
+        $webcie = Committee::where('unique_name', 'webcie')->first();
+        $loran->committees()->save($webcie, ['start_date' => Carbon::now()]);
+
+        // Fake users with profile
         User::factory()
             ->profileType()
             ->hasProfile(1, function (array $attributes, User $user) use ($faker, $yearGroupIds) {
@@ -30,6 +49,7 @@ class UserSeeder extends Seeder
             ->count($this->numUsersWithProfiles)
             ->create();
 
+        // Fake users without profile
         User::factory()
             ->noProfileType()
             ->count($this->totalUsers - $this->numUsersWithProfiles)
