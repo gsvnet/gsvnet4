15263a632240020c84ee7110766686ad776f6bc6
lasschehj <lassche.hj@gmail.com>
2023-10-02 22:58:41 +0200

    Make committees responsive

--- resources/views/committees/index.blade.php
+++ resources/views/committees/index.blade.php
@@ -6,7 +6,7 @@
               <x-committees.selection-button text="Senaten"/>
        </div>
 
-       <div class="pt-4 grid grid-cols-4 gap-x-2 gap-y-4">
+       <div class="pt-4 grid lg:grid-cols-4 grid-cols-3 gap-x-2 gap-y-4">
               @foreach ($committees as $committee)
                      <x-committees.show-card title="{{ $committee->name }}" description="{{ $committee->description }}" unique="{{ $committee->unique_name }}" />
               @endforeach
--- resources/views/committees/show.blade.php
+++ resources/views/committees/show.blade.php
@@ -14,7 +14,7 @@
        <h2 class="pt-2 text-xs px-4">
        Actieve Leden:
        </h2>
-       <div class="grid grid-cols-8 pt-2 gap-2 px-6">
+       <div class="grid sm:grid-cols-6 lg:grid-cols-8 pt-2 gap-2 px-6 grid-cols-4">
               @foreach ($activeMembers as $member)
                      <x-committees.show-member :member=$member/>
               @endforeach
@@ -23,7 +23,7 @@
        <h2 class="pt-8 text-xs px-4">
        Leden Afgelopen Twee Jaar:
        </h2>       </h2>
-       <div class="grid grid-cols-12 pt-2 gap-2 px-6">
+       <div class="grid lg:grid-cols-12 sm:grid-cols-8 grid-cols-6 sm pt-2 gap-2 px-6">
               @foreach ($previousMembers as $member)
                      <x-committees.show-previous-member :member=$member/>
               @endforeach
