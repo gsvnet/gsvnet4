807c090691e004023a4ceece25964c0aa3729c57
lasschehj <lassche.hj@gmail.com>
2023-10-11 17:25:20 +0200

    Reformat searchbar

---
+++ resources/views/components/navigation/mobile/menu-item.blade.php
@@ -0,0 +1,6 @@
+<li class="hover:border-l-[#c5023d] hover:cursor-pointer border-l-2 border-l-transparent hover:bg-gradient-to-r hover:from-gsv-purple/80 hover:to-transparent w-full h-full pt-2 pb-2 mb-2 transition-all duration-300 ease-in">
+    <a class="flex flex-row justify-center" href="{{ $link }}">
+        {{ $slot }}
+        {{ $title }}
+    </a>
+</li>
