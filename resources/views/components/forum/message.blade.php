1f0f0ed57a070e5c7c9b305d9861cc6fc2958d10
lasschehj <lassche.hj@gmail.com>
2023-10-17 15:41:39 +0200

    Smally change

--- resources/views/components/forum/message.blade.php
+++ resources/views/components/forum/message.blade.php
@@ -21,9 +21,12 @@
         <p class="font-bold">
             Pieter Roon 
         </p>
-        <p class="text-xs">
-            5 uur geleden - bewerk - verwijder
-        </p>
+        <div class="text-xs flex flex-row">
+            <p>5 uur geleden-</p>
+            <p class="hover:underline hover:cursor-text">bewerk 
+            <p>-</p>
+            <p class="hover:underline hover:cursor-text">verwijder</p>
+        </div>
         <div class="mt-2 mr-2">
             <p class="font-normal text-gray-700 dark:text-gray-400"> 
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
