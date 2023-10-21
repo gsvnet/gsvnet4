fef100ec7ae151a673e16b328931b000e5b21cd9
Valkje <loran.knol@gmail.com>
2023-10-21 21:06:37 +0200

    Enable testing

--- tests/Feature/MemberTest.php
+++ tests/Feature/MemberTest.php
@@ -2,19 +2,58 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use GSVnet\Newsletters\NewsletterList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Mockery\MockInterface;
 
 class MemberTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_name_can_be_changed(): void
     {
-        $response = $this->get('forum.localhost');
+        // Changing a member's name will invoke the subscribeTo method on
+        // the NewsletterList interface, but we don't want this method to
+        // activate during testing (because it will try to communicate with 
+        // the Mailchimp server). That's why we mock it.
+        $this->mock(NewsletterList::class, function (MockInterface $mock) {
+            $mock->shouldReceive("subscribeTo")->once();
+        });
+        
+        $manager = User::factory()->create();
 
-        $response->assertStatus(200);
+        $user = User::factory()
+            ->profileType()
+            ->hasProfile(1, function (array $attributes, User $user) {
+                return ['year_group_id' => 1];
+            })
+            ->create();
+
+        $initials = "J.J.";
+        $firstname = "Jan";
+        $middlename = "van";
+        $lastname = "Jansen";
+        
+        $response = $this->actingAs($manager)
+                         ->put('admin/leden/'.$user->id.'/naam', [
+                            "initials" => "J.J.",
+                            "firstname" => "Jan",
+                            "middlename" => "van",
+                            "lastname" => "Jansen",
+                         ]);
+
+        // These requests always redirect to UsersController@show
+        $response->assertStatus(302);
+
+        // Retrieve user from database and check that the update worked
+        $user = User::find($user->id);
+        $this->assertEquals($initials, $user->profile->initials);
+        $this->assertEquals($firstname, $user->firstname);
+        $this->assertEquals($middlename, $user->middlename);
+        $this->assertEquals($lastname, $user->lastname);
     }
 }
