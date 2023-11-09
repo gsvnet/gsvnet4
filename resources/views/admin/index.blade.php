505c2ff58c68d2ce5f99f182c62baf5d2b73de5d
lasschehj <lassche.hj@gmail.com>
2023-11-09 20:17:26 +0100

    Finalize dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,13 +13,13 @@
             Snelle dingetjes
         </h3>
         <div class="mt-4">
-            <button class="bg-gsv-purple hover:bg-gsv-purple/80 dark:bg-gsv-purple-dark dark:hover:bg-gsv-purple-dark/60 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+            <button href="/admin/leden/leden.csv" class="bg-gsv-purple hover:bg-gsv-purple/80 dark:bg-gsv-purple-dark dark:hover:bg-gsv-purple-dark/60 text-white font-bold py-2 px-4 rounded inline-flex items-center">
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                 <span>Ledenbestand (csv)</span>
             </button>
         </div>
         <div class="mt-4">
-            <button class="bg-gsv-purple hover:bg-gsv-purple/80 dark:bg-gsv-purple-dark dark:hover:bg-gsv-purple-dark/60 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+            <button href="/admin/leden/sic-ontvangers.csv" class="bg-gsv-purple hover:bg-gsv-purple/80 dark:bg-gsv-purple-dark dark:hover:bg-gsv-purple-dark/60 text-white font-bold py-2 px-4 rounded inline-flex items-center">
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                 <span>SIC-ontvangers (csv)</span>
             </button>
