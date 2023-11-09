e0311a6e8be7b23b05e26a7896e330c87dc329e3
lasschehj <lassche.hj@gmail.com>
2023-11-09 20:16:18 +0100

    Admin darkmode

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')    
-    <div class="text-black">
+    <div class="text-black dark:text-white">
         <h1 class="text-3xl divide-black border-b-2 border-gray-300 mb-4">
             GSVnet van achter
         </h1>
@@ -13,13 +13,13 @@
             Snelle dingetjes
         </h3>
         <div class="mt-4">
-            <button class="bg-gsv-purple hover:bg-gsv-purple/80 dark:bg-gsv-purple-dark dark:hover:bg-gsv-purple-dark/80 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+            <button class="bg-gsv-purple hover:bg-gsv-purple/80 dark:bg-gsv-purple-dark dark:hover:bg-gsv-purple-dark/60 text-white font-bold py-2 px-4 rounded inline-flex items-center">
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                 <span>Ledenbestand (csv)</span>
             </button>
         </div>
         <div class="mt-4">
-            <button class="bg-gsv-purple hover:bg-gsv-purple/80 dark:bg-gsv-purple-dark dark:hover:bg-gsv-purple-dark/80 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+            <button class="bg-gsv-purple hover:bg-gsv-purple/80 dark:bg-gsv-purple-dark dark:hover:bg-gsv-purple-dark/60 text-white font-bold py-2 px-4 rounded inline-flex items-center">
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                 <span>SIC-ontvangers (csv)</span>
             </button>
--- resources/views/layouts/navigation-admin.blade.php
+++ resources/views/layouts/navigation-admin.blade.php
@@ -6,9 +6,12 @@
     
     <nav>
         <h3 class="text-xl text-black dark:text-white font-extralight ml-4 mt-6">
-        Administratie
+            Administratie
         </h3>
         <ul class="dark:text-white text-black mt-2 pt-4 dark:bg-[#202124] bg-[#d0d5d6] rounded-2xl ml-3 mr-3 pb-6 text-center">
+            <div class="items-center flex justify-center mb-6 mt-2">
+                <x-navigation.dark-toggle/>
+            </div>
             <x-navigation.menu-item title="Dashboard" link="/admin"> 
                 <svg fill="none" class="h-6 w-6 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3.1875L21.4501 10.275L21.0001 11.625H20.25V20.25H3.75005V11.625H3.00005L2.55005 10.275L12 3.1875ZM5.25005 10.125V18.75H18.75V10.125L12 5.0625L5.25005 10.125Z" fill="currentColor"/>
@@ -85,7 +88,7 @@
             <div class="items-center justify-center flex">
                 <form method="POST" action="{{ route('uitloggen') }}">
                     @csrf
-    
+                    
                     <button type="submit" class="flex flex-row dark:text-white text-black ml-6 mt-2 hover:cursor-pointer hover:text-black/70 dark:hover:text-white/70">
                         <p class="text-xs my-auto mr-2 mx-auto">
                             Log uit
