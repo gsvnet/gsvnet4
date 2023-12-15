2a0939d64e181385c222eefa2c6b77bfb9764a17
lasschehj <lassche.hj@gmail.com>
2023-12-15 14:19:22 +0100

    Changes navbar admin

--- resources/views/layouts/navigation-mobile.blade.php
+++ resources/views/layouts/navigation-mobile.blade.php
@@ -42,7 +42,7 @@
                     <path d="M17 12V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg> 
             </x-navigation.mobile.menu-item> 
-            <x-navigation.mobile.menu-item title="Admin" link="/"> 
+            <x-navigation.mobile.menu-item title="Admin" link="/admin"> 
                 <svg fill="none" class="h-6 w-6 mr-2" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
                     <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>    
